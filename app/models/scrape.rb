5c56b9e3767260ecbaccfa143a3ea9b194a7d9ef
Simón Muñoz <simonvlc@gmail.com>
2016-10-02 19:04:00 +0200

    fixing filename

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,9 +1,9 @@
 class Scrape
-  attr_accessor :title, :hotness, :image_url, :rating, :director, :genre, :runtime, :synopsys, :failure
+  attr_accessor :title, :hotness, :image_url, :rating, :director, :genre, :runtime, :synopsis, :failure
 
   def scrape_new_movie
     begin
-      doc = Nokogiri::HTML(open("http://www.rottentomatoes.com/m/the_martian/"))
+      doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/the_martian/"))
       doc.css('script').remove
       self.title = doc.css("//h1.title.hidden-xs").text.strip
       self.hotness = doc.css("//.meter-value").text.to_i
@@ -18,12 +18,26 @@ class Scrape
       if ! s.valid_encoding?
         s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
       end
-      self.synopsys = s
-
+      self.synopsis = s
 
       return true
     rescue Exception => e
-      self.failure = "Something went wrong with the scrape"
+      self.failure = e
     end
   end
-end
+
+  def save_movie
+    movie = Movie.new(
+      title: self.title,
+      hotness: self.hotness,
+      image_url: self.image_url,
+      synopsis: self.synopsis,
+      rating: self.rating,
+      genre: self.genre,
+      director: self.director,
+      runtime: self.runtime
+      )
+    movie.save
+  end
+
+end # class
